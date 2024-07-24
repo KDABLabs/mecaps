31da36ceeba647a1539952828487c0ba95785870
Marco Thaller <marco.thaller@kdab.com>
2024-07-24 09:39:51 +0200

    build: bump dependencies
    curl -> v8.9.0
    https://curl.se/changes.html#8_9_0
    
    slint -> v1.7.0
    https://github.com/slint-ui/slint/blob/master/CHANGELOG.md#170---2024-07-18

--- cmake/dependencies/curl.cmake
+++ cmake/dependencies/curl.cmake
@@ -7,7 +7,7 @@ if (NOT TARGET CURL::libcurl)
     FetchContent_Declare(
         curl
         GIT_REPOSITORY https://github.com/curl/curl.git
-        GIT_TAG curl-8_8_0
+        GIT_TAG curl-8_9_0
     )
     set(BUILD_TESTING OFF CACHE BOOL "Turn off testing" FORCE)
     set(BUILD_CURL_EXE OFF CACHE BOOL "Turn off curl executable" FORCE)
