2d3805ab303cef3dfcb06f3c1e1e4b6090df84d3
Sean Harmer <sean.harmer@kdab.com>
2025-07-22 10:27:54 +0100

    Bump slint to latest v1.12.1
    Needs a recent msvc compiler if building on windows to avoid errors
    in skia.

--- cmake/dependencies/slint.cmake
+++ cmake/dependencies/slint.cmake
@@ -1,7 +1,9 @@
 find_package(Slint QUIET)
 
-if (NOT Slint_FOUND)
-  message("Slint could not be located in the CMake module search path. Downloading it from Git and building it locally")
+if(NOT Slint_FOUND)
+  message(
+    "Slint could not be located in the CMake module search path. Downloading it from Git and building it locally"
+  )
 
   # set(SLINT_FEATURE_BACKEND_WINIT OFF) # this removes X11 support
   set(SLINT_FEATURE_BACKEND_QT OFF)
@@ -13,8 +15,7 @@ if (NOT Slint_FOUND)
     GIT_REPOSITORY https://github.com/slint-ui/slint.git
     # `release/1` will auto-upgrade to the latest Slint >= 1.0.0 and < 2.0.0
     # `release/1.0` will auto-upgrade to the latest Slint >= 1.0.0 and < 1.1.0
-    GIT_TAG v1.7.0
-    SOURCE_SUBDIR api/cpp
-  )
+    GIT_TAG v1.12.1
+    SOURCE_SUBDIR api/cpp)
   FetchContent_MakeAvailable(Slint)
-endif (NOT Slint_FOUND)
+endif(NOT Slint_FOUND)
